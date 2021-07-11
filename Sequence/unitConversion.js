1bc6e632142e5d8a92add88c2d696fd1f6152979
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-07-11 17:34:24 +0530

    added : unit conversion feets into inch and feets into meters

---
+++ Sequence/unitConversion.js
@@ -0,0 +1,13 @@
+//a). 1ft = 12 in then 42 in = ? ft
+
+let oneFeetInInch = 12;
+let result = 42/oneFeetInInch;
+
+console.log("42 inches in feets = " + result);
+
+//b). Rectangular Plot of 60 feet x 40 feet in meters
+
+let areaInFeet = 60 * 40;
+let areaInMeter = areaInFeet/3.28084;
+
+console.log("60 * 40 ft Area in meter : " + areaInMeter);
